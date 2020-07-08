61267560a521e16632b196945a104611503fad71
stefanovallarella <stefanovallarella@gmail.com>
2020-07-08 18:31:01 -0300

    Actualizacion Register / login + POST data.json

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const express = require ("express");
 const app = express();
 
@@ -5,14 +6,47 @@ const app = express();
 // El use me va a permitir configurar express    -- > usando la carpeta public le dice a express que la use directamente y no pase por todo el sistema de express
 /* app.use(express.static("NOMBRE DE LA CARPETA")); */
 // sabe donde está la carpeta public porque está en la misma raíz de mi proyecto 
+
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
 
 
 app.get("/", (req, res) => {
 
     res.sendFile(__dirname + "/public/intro-a-html/index.html");
 
-})
+});
+
+app.post("/register", (req , res) => {
+
+
+    let data = JSON.stringify(req.body, null, " ");
+    fs.writeFileSync("data.json", data);
+
+    // res.send(req.body);
+
+    res.sendFile(__dirname + "/public/intro-a-html/index.html");
+
+});
+
+
+
+
+app.get("/register", (req , res) =>{
+
+    res.sendFile(__dirname + "/public/intro-a-html/register.html");
+
+});  
+
+
+app.get("/login", (req , res) =>{
+
+    res.sendFile(__dirname + "/public/intro-a-html/login.html");
+
+});  
+
+
+
 
 
 console.log(__dirname);
